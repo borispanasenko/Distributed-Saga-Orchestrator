948d6185a8f6c03cd22ad1278b1069029098afc8
borispanasenko <4dspiral@gmail.com>
2026-01-16 03:38:16 +0200

    docs: fix idempotency key format in comments to match implementation

--- src/SagaOrchestrator.Domain/Entities/IdempotencyKey.cs
+++ src/SagaOrchestrator.Domain/Entities/IdempotencyKey.cs
@@ -2,7 +2,7 @@ namespace SagaOrchestrator.Domain.Entities;
 
 public class IdempotencyKey
 {
-    // Unique key: "{SagaId}_{StepName}"
+    // Unique key: "{StepName}_{SagaId}"
     public string Key { get; set; } = string.Empty;
     
     // DateTime of occasion
